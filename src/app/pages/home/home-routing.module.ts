0a441747d68a3675565ac967bd5552ca431afd14
BenjaminDuran <102270402+BenjaminDuran@users.noreply.github.com>
2022-10-21 22:52:19 -0300

    a

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from "../../components/inicio/inicio.component"
 import { MiClaseComponent } from "../../components/mi-clase/mi-clase.component"
+import { ApiComponent } from "../../components/api/api.component"
 
 import { HomePage } from './home.page';
 
@@ -17,6 +18,10 @@ const routes: Routes = [
       {
         path: "mi-clase",
         component: MiClaseComponent
+      },
+      {
+        path: "api",
+        component: ApiComponent
       }
     ]
   },
